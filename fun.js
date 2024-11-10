ee962b407db89b73a2fc9213b491584d7abe1e70
Sahand <sahandmustafa005@gmail.com>
2024-11-10 19:42:38 +0300

    adding pop up order screen

--- fun.js
+++ fun.js
@@ -1,1 +1,8 @@
-console.log('Server run'); // test if the js run along the html
+console.log('Server run'); // test if the js runs along with the html
+const orderScreen = document.getElementById('orderScreen')
+function order(name){
+ console.log(name)
+     orderScreen.style.display='flex'
+     var food = document.forms['myForm']['food'];
+    food.value = name;
+}
