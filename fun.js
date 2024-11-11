9a79e8a6840aa86e9b2b6d39d69c1f48f827f776
Sahand <sahandmustafa005@gmail.com>
2024-11-11 22:02:35 +0300

    removing the pop up and making Cart and add to cart function, removing different size only to 1 size,

--- fun.js
+++ fun.js
@@ -1,8 +1,27 @@
 console.log('Server run'); // test if the js runs along with the html
-const orderScreen = document.getElementById('orderScreen')
-function order(name){
- console.log(name)
-     orderScreen.style.display='flex'
-     var food = document.forms['myForm']['food'];
-    food.value = name;
+const cart = document.getElementById('Cart');
+const finalPriceDiv = document.getElementById('finalPrice');
+
+let jsFinalPrice = 0;
+let basket = [];
+
+
+function order(name, price){
+ let item = name;
+ let pricey = price;
+ basket.push(item);
+ console.log(basket); 
+ let foodName = document.createElement('h1');
+ let foodPrice = document.createElement('h1');
+ let divy = document.createElement('div');
+ foodName.innerHTML=(item);
+ foodPrice.innerHTML=("Price:" +  pricey + "$");
+ let firstH1 = cart.querySelector('h1'); 
+ cart.insertBefore(divy, firstH1.nextSibling);
+ divy.append(foodName);
+ divy.append(foodPrice);
+ let finalPriceText = document.getElementById('finalPriceText');
+ jsFinalPrice = jsFinalPrice + pricey;
+ let roundedPrice = jsFinalPrice.toFixed(2);
+ finalPriceText.innerHTML=(roundedPrice + '$')
 }
